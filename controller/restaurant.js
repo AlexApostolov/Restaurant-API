8a796756e133be88fddc00d643af564b6385aa5e
Alexander Apostolov <emailme@alexanderapostolov.com>
2017-05-03 12:27:29 -0400

    UPDATE and DELETE controllers added

--- controller/restaurant.js
+++ controller/restaurant.js
@@ -5,7 +5,8 @@ import Restaurant from '../model/restaurant';
 export default({config, db}) => {
   let api = Router();
 
-  // '/v1/restaurant/add'
+  // CREATE
+  // add new restaurant '/vi1/restaurant/add'
   api.post('/add', (req, res) => {
     let newRest = new Restaurant();
     newRest.name = req.body.name;
@@ -17,6 +18,58 @@ export default({config, db}) => {
       res.json({message: 'Restaurant saved successfully'});
     });
   });
+
+  // READ
+  // retrieve all restaurants '/vi1/restaurant'
+  api.get('/', (req, res) => {
+    Restaurant.find({}, (err, restaurants) => {
+      if(err) {
+        res.send(err);
+      }
+      res.json(restaurants);
+    });
+  });
+
+  // retrieve 1 restaurant '/vi1/restaurant/:id'
+  api.get('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if(err) {
+        res.send(err);
+      }
+      res.json(restaurant);
+    });
+  });
+
+  // UPDATE
+  // update existing restaurant '/vi1/restaurant/:id'
+  api.put('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err,restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      restaurant.name = req.body.name;
+      restaurant.save(err => {
+        if(err) {
+          res.send(err);
+        }
+        res.json({message: "Restaurant info updated"});
+      });
+    });
+  });
+
+  // DELETE
+  // delete restaurant '/v1/restaurant/:id'
+  api.delete('/:id', (req, res) => {
+    Restaurant.remove({
+      _id: req.params.id
+    }, (err, restaurant) => {
+      if(err) {
+        res.send(err);
+      }
+      res.json({message: "Restaurant Successfully Removed!"});
+    });
+  });
+
 
   return api;
 }
